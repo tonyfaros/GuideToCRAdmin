0273e66e42b5ac9980c3e1a67e77b5668b2cf6f3
Maria <marilau63@gmail.com>
2018-06-08 01:00:31 -0600

    no message

--- administrador/app/controllers/users/registrations_controller.rb
+++ administrador/app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-    skip_before_filter :verify_authenticity_token, :only => :create
-    
+    skip_before_action :verify_authenticity_token, :only => :create
+
   end
