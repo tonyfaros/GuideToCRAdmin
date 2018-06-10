fa4ab1f586f65c1aad57be583e89b7160b491a52
Maria <marilau63@gmail.com>
2018-06-09 23:39:47 -0600

    no message

--- administrador/db/migrate/20180610024123_remove_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :encrypted_password, :string
-    remove_column :users, :reset_password_token, :string
-    remove_column :users, :reset_password_sent_at, :string
-    remove_column :users, :remember_created_at, :string
-  end
-end
--- administrador/db/migrate/20180610052027_remove_userr.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUserr < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :nombre, :string
-    remove_column :users, :image, :string
-    remove_column :users, :authentication_token, :string
-  end
-end
